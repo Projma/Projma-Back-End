eddcd47481c13f2c122d1dcb66220ef828afa2a8
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-02 21:03:37 +0330

    adding swagger

--- ProjmaBackend/settings.py
+++ ProjmaBackend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'accounts',
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- ProjmaBackend/urls.py
+++ ProjmaBackend/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Projma API",
+        default_version='v1',
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger')),
 ]
