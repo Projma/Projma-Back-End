9823d2872fa50aa7d12ced6fd5d9aa5db35bfe3e
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-02 17:45:46 +0330

    modify url of signup and login in account app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,16 @@
 from django.urls import include, path
-from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from .routers import *
 from .models import *
 from .views import *
 
 router = UserRouter()
-router.register('users', UserViewset, basename='user')
+router.register('users', UserViewSet, basename='user')
 
+urlpatterns = router.urls
 
-urlpatterns = [
-    path('', include(router.urls))
+urlpatterns += [
+    path('login/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
