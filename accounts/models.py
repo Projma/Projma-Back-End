0d0fe62b8343d444f83d1e627908f6f3dd6d031e
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2022-11-14 19:23:13 +0330

    [fix]: resolve conflict - rebasing feat-proflie with develop

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
@@ -18,4 +19,11 @@ class User(AbstractUser):
         if self.is_superuser:
             self.is_active = True
         return super().save(*args, **kwargs)
+
+class Profile(models.Model):
+    bdate = models.DateField()
+    bio = models.CharField(max_length=1000)
+    phone = models.CharField()
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
+    img = models.ImageField()
 
