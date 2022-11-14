ce82cef182e582115c6e633a3db40dae0b0d263f
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-14 22:54:50 +0330

    [chore]: editing database settings

--- ProjmaBackend/settings.py
+++ ProjmaBackend/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'ProjmaBackend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ProjmaDB', 
-        'USER': 'postgres',
-        'PASSWORD': 'classicguys',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
+        'NAME': env('DB_NAME'), 
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'), 
+        'PORT': env('DB_PORT'),
     }
 }
 
