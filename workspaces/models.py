1d127cb67b6e81451a177f74f55ef5029a787518
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-14 22:30:03 +0330

    [feat]: adding board model

--- workspaces/models.py
+++ workspaces/models.py
@@ -21,3 +21,10 @@ class WorkSpace(models.Model):
     type = models.CharField(max_length=256, choices=TYPE_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    background_pic = models.URLField(max_length=1000, blank=True, null=True)
+    workspace = models.ForeignKey(WorkSpace, on_delete=models.CASCADE)
