045059a76d2dc49372be6588887fdbc40605e0ad
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-14 22:29:11 +0330

    [feat]: adding workspace model

--- workspaces/models.py
+++ workspaces/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from accounts.models import Profile
+
 
 # Create your models here.
+class WorkSpace(models.Model):
+    TYPE_CHOICES = [
+        ("education", "Education"),
+        ("marketing", "Marketing"),
+        ("small business", "Small Business"),
+        ("sales & crm", "Sales & CRM"),
+        ("operations", "Operations"),
+        ("engineering-it", "Engineering IT"),
+        ("finance", "Finance"),
+        ("human resources", "Human Resources"),
+        ("other", "Other"),
+    ]
+
+    name = models.CharField(max_length=256)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    type = models.CharField(max_length=256, choices=TYPE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
