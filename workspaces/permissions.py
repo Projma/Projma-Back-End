2e43f90592916b26d059bdfcb50bc93fad0368a6
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-16 22:59:27 +0330

    [feat]: workspace permissions: personalize getting workspace

--- workspaces/permissions.py
+++ workspaces/permissions.py
@@ -3,17 +3,15 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class WorkSpacePermissions(BasePermission):
     def has_permission(self, request, view):
-        if request.user and request.user.is_staff:
-            return True
-        if not request.user or request.user.is_anonymous:
-            return False
-        if request.method == 'POST' or request.method == 'DELETE':
+        if request.method == 'POST':
             return bool(request.user and request.user.is_authenticated)
+        elif request.method == 'GET':
+            return bool(request.user and request.user.is_staff)
         return True
 
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser or request.user.is_staff:
-            return True
-        elif obj.owner.user == request.user:
-            return True
-        return False
+    # def has_object_permission(self, request, view, obj):
+    #     if request.user.is_superuser or request.user.is_staff:
+    #         return True
+    #     elif obj.owner.user == request.user:
+    #         return True
+    #     return False
