5e0887c7a923f21c04bd6c95e9e1cde4f83b7c7a
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-16 17:25:32 +0330

    [feat]: workspaace permissions

--- workspaces/views.py
+++ workspaces/views.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
 from .models import WorkSpace
 from .serializers import WorkspaceSerializer
+from .permissions import WorkSpacePermissions
 # Create your views here.
 
 class WorkSpaceViewSet(viewsets.ModelViewSet):
     queryset = WorkSpace.objects.all()
     serializer_class = WorkspaceSerializer
+    permission_classes = [WorkSpacePermissions]
