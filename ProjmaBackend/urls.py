d89d50772000fa4c4dd9334c795c30d96fd8b8da
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-16 15:39:53 +0330

    [feat]: init workspace urls

--- ProjmaBackend/urls.py
+++ ProjmaBackend/urls.py
@@ -29,6 +29,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('workspaces/', include('workspaces.urls')),
 
     path('swagger/', schema_view.with_ui('swagger')),
 ]
