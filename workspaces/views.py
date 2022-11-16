5ead541e888968e93eaf99cd3c55c6fcfdf8ffea
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-16 23:21:32 +0330

    [feat]: retrieve all workspaces for a specific user

--- workspaces/views.py
+++ workspaces/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import WorkSpace
 from .serializers import WorkspaceSerializer
 from .permissions import WorkSpacePermissions
@@ -8,3 +9,11 @@ class WorkSpaceViewSet(viewsets.ModelViewSet):
     queryset = WorkSpace.objects.all()
     serializer_class = WorkspaceSerializer
     permission_classes = [WorkSpacePermissions]
+
+    def list(self, request, *args, **kwargs):
+        user = request.user
+        if request.user.is_staff:
+            serializer = self.get_serializer(self.get_queryset(), many=True)
+        else:
+            serializer = self.get_serializer(self.queryset.filter(owner__user=user), many=True)
+        return Response(serializer.data)
