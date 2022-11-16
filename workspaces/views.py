8852ce030bd285c3e6a6bf193c34147fe49c5d5b
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-16 15:32:31 +0330

    [feat]: workspace viewset

--- workspaces/views.py
+++ workspaces/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import WorkSpace
+from .serializers import WorkspaceSerializer
 # Create your views here.
+
+class WorkSpaceViewSet(viewsets.ModelViewSet):
+    queryset = WorkSpace.objects.all()
+    serializer_class = WorkspaceSerializer
