cce04ac1ed1c4b8abf13c9651414fc06901acb63
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-16 23:12:52 +0330

    [fix]: overriding create method in workspace serializer.(pick up the owner automatically)

--- workspaces/serializers.py
+++ workspaces/serializers.py
@@ -1,9 +1,20 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-from .models import WorkSpace
+from .models import WorkSpace, Profile
 
 class WorkspaceSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
+    owner = serializers.PrimaryKeyRelatedField(read_only=True,)
     class Meta:
         model = WorkSpace
         fields = ['id', 'name', 'description', 'type', 'created_at', 'owner']
+
+    def create(self, validated_data):
+        try:
+            user = self.context['request'].user
+        except:
+            raise serializers.ValidationError("User not found")
+        profile = get_object_or_404(Profile, user=user)
+        validated_data['owner'] = profile
+        return super().create(validated_data)
