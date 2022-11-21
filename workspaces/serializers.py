24d5818bc3522f078cb6a5119a6591d42a5056e8
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-22 02:35:30 +0330

    [fix]: fixing workspace serializer and make boards read_only

--- workspaces/serializers.py
+++ workspaces/serializers.py
@@ -5,7 +5,8 @@ from .models import WorkSpace, Profile, Board
 class WorkspaceSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
     created_at = serializers.DateTimeField(read_only=True)
-    owner = serializers.PrimaryKeyRelatedField(read_only=True,)
+    owner = serializers.PrimaryKeyRelatedField(read_only=True)
+    boards = serializers.PrimaryKeyRelatedField(read_only = True, many=True)
     class Meta:
         model = WorkSpace
         fields = ['id', 'name', 'description', 'type', 'created_at', 'updated_at', 'owner', 'members', 'boards']
@@ -25,7 +26,8 @@ class BoardSerializer(serializers.ModelSerializer):
     workspace = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Board
-        fields = ['id', 'name', 'description', 'background_pic', 'workspace', 'admins', 'created_at', 'updated_at', 'members', 'tasklists']
+        fields = ['id', 'name', 'description', 'background_pic', 'workspace', 'admins', 
+                    'created_at', 'updated_at', 'members', 'tasklists']
 
     def create(self, validated_data):
         request = self.context['request']
