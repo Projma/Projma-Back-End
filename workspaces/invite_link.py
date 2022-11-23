2535702ffe70d324db93baf2db5735e53f6cf69c
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-23 17:57:17 +0330

    [fix]: crud workspace for owner

--- workspaces/invite_link.py
+++ workspaces/invite_link.py
@@ -6,7 +6,8 @@ def encode(workspace: WorkSpace) -> str:
     id_encoded = f'6289{workspace.pk}'
     name_encoded = ''.join([chr(int(c) + 97) for c in str(hash(workspace.name) % 100000)])
     range_nums = [i for i in range(48, 58)] + [i for i in range(65, 91)] + [i for i in range(97, 123)]
-    random_part = ''.join([chr(random.choice(range_nums)) for _ in range(8)])
+    # random_part = ''.join([chr(random.choice(range_nums)) for _ in range(8)])
+    random_part = ''.join([str(i+5) for i in range(4)])
     return f'{name_encoded}-{id_encoded}${workspace.name}{random_part}'
 
 
--- workspaces/urls.py
+++ workspaces/urls.py
@@ -6,7 +6,6 @@ router = DefaultRouter()
 
 router.register('workspaces', views.WorkspaceViewSet, basename='workspaces')
 router.register(r'workspaces/(?P<w_id>[^/.]+)/boards', views.BoardManagementViewSet, basename='boards')
-router.register('boards', views.BoardViewSet, basename='boards')
 router.register('dashboard', views.UserDashboardViewset, basename='dashboard')
 router.register('workspaceowner', views.WorkSpaceOwnerViewSet, basename='workspaceowner')
 
