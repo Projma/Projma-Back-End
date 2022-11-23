aacf3a85c3c02df6a2ab5c1d4feb8148df503666
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-11-23 20:58:39 +0330

    [fix]: resolve conflict

--- workspaces/urls.py
+++ workspaces/urls.py
@@ -5,7 +5,8 @@ from . import views
 router = DefaultRouter()
 
 router.register('workspaces', views.WorkspaceViewSet, basename='workspaces')
-router.register(r'(?P<w_id>[^/.]+)/boardadmin', views.BoardAdminViewSet, basename='boardsadmin')
+router.register(r'(?P<w_id>[^/.]+)/boardsadmin', views.BoardAdminViewSet, basename='boardsadmin')
+router.register(r'(?P<w_id>[^/.]+)/boardsmember', views.BoardMembershipViewSet, basename='boardsmember')
 router.register('dashboard', views.UserDashboardViewset, basename='dashboard')
 router.register('workspaceowner', views.WorkSpaceOwnerViewSet, basename='workspaceowner')
 
