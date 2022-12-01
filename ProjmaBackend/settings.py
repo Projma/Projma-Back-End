e158618904e654dee61266a4bcb7e8913cfe49b3
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-12-01 17:20:28 +0330

    [feat]: search user with their username or email

--- ProjmaBackend/settings.py
+++ ProjmaBackend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
