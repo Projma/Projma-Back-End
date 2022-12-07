36a813f27e8ac169ed03a6c841c456474c7ad3b4
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2022-12-08 01:46:40 +0330

    [chore]: fake commit

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+# Create your models here. 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(
