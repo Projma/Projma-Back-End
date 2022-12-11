931640beee72ae266863a06b65052296fb7ab994
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-12-11 22:30:52 +0330

    [feat]: create kanban board template

--- workspaces/views/templates.py
+++ workspaces/views/templates.py
@@ -30,24 +30,46 @@ class TemplateViewSet(viewsets.GenericViewSet):
             label_color = label_param['color']
             labels.append(Label.objects.create(title=label_title, color=label_color, board_template=board_template))
 
-    # @action(detail=False, methods=['get'], url_path='create-project-management-template')
-    # def create_project_management_template(self, request):
-    #     template_params = {'name': 'Project Management', 'description': 'Project Management Template'}
-    #     tasklists_params = [{'title': 'Project Resources'}, \
-    #             {'title': 'Questions For Next Meeting'}, \
-    #             {'title': 'Project Progress'}, \
-    #             {'title': 'To Do'}, \
-    #             {'title': 'Pending'}, \
-    #             {'title': 'Blocked'}, \
-    #             {'title': 'Done'}]
-    #     labels_params = [{'title': 'Copy Request', 'color': '#D6FF3680'}, \
-    #             {'title': 'Priority', 'color': '#FF805980'}, \
-    #             {'title': 'Design Team', 'color': '#C712BA87'}]
-    #     self.create_template(template_params, tasklists_params, labels_params)
-    #     return Response(status=status.HTTP_201_CREATED)
+    @action(detail=False, methods=['get'], url_path='create-project-management-template')
+    def create_project_management_template(self, request):
+        template_params = {'name': 'Project Management', 'description': 'Project Management Template'}
+        tasklists_params = [{'title': 'Project Resources'}, \
+                {'title': 'Questions For Next Meeting'}, \
+                {'title': 'Project Progress'}, \
+                {'title': 'To Do'}, \
+                {'title': 'Pending'}, \
+                {'title': 'Blocked'}, \
+                {'title': 'Done'}]
+        labels_params = [{'title': 'Copy Request', 'color': '#D6FF3680'}, \
+                {'title': 'Priority', 'color': '#FF805980'}, \
+                {'title': 'Design Team', 'color': '#C712BA87'}]
+        self.create_template(template_params, tasklists_params, labels_params)
+        return Response(status=status.HTTP_201_CREATED)
+
+    @action(detail=False, methods=['get'], url_path='create-kanban-template')
+    def create_kanban_template(self, request):
+        template_params = {'name': 'Kanban', 'description': 'Kanban Template'}
+        tasklists_params = [{'title': 'Backlog'}, \
+                {'title': 'Design'}, \
+                {'title': 'To Do'}, \
+                {'title': 'Doing'}, \
+                {'title': 'Code Review'}, \
+                {'title': 'Testing'},
+                {'title': 'Done'}]
+        labels_params = [{'title': 'In Queue', 'color': '#FF78FA96'}, \
+                {'title': 'In Progress', 'color': '#FFCD2696'}, \
+                {'title': 'Completed', 'color': '#66FF0F96'}]
+        self.create_template(template_params, tasklists_params, labels_params)
+        return Response(status=status.HTTP_201_CREATED)
 
     @action(detail=False, methods=['get'], url_path='project-management')
     def project_management(self, request):
         template = get_object_or_404(BoardTemplate, name='Project Management')
+        serializer = self.get_serializer(template)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'], url_path='kanban')
+    def kanban(self, request):
+        template = get_object_or_404(BoardTemplate, name='Kanban')
         serializer = self.get_serializer(template)
         return Response(serializer.data, status=status.HTTP_200_OK)
