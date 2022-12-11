378505f7fd76bba4619c5bf5e21aa5e538ec73ff
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-12-11 22:44:03 +0330

    [feat]: create simple board template

--- workspaces/views/templates.py
+++ workspaces/views/templates.py
@@ -16,18 +16,18 @@ class TemplateViewSet(viewsets.GenericViewSet):
         tasklists_params = [{title:...}, ...]
         labels_params = [{title:..., color:...}, ...]
         '''
-        template_name = template_params['name']
-        template_description = template_params['description']
+        template_name = template_params.get('name')
+        template_description = template_params.get('description')
         board_template = BoardTemplate.objects.create(name=template_name, description=template_description)
         tasklists = []
         labels = []
         for tasklist_param in tasklists_params:
-            tasklist_title = tasklist_param['title']
+            tasklist_title = tasklist_param.get('title')
             tasklists.append(TaskList.objects.create(title=tasklist_title, board_template=board_template))
 
         for label_param in labels_params:
-            label_title = label_param['title']
-            label_color = label_param['color']
+            label_title = label_param.get('title')
+            label_color = label_param.get('color')
             labels.append(Label.objects.create(title=label_title, color=label_color, board_template=board_template))
 
     @action(detail=False, methods=['post'], url_path='create-project-management-template')
@@ -79,6 +79,17 @@ class TemplateViewSet(viewsets.GenericViewSet):
         self.create_template(template_params, tasklists_params, labels_params)
         return Response(status=status.HTTP_201_CREATED)
 
+    @action(detail=False, methods=['post'], url_path='create-simple-template')
+    def create_simple_template(self, request):
+        template_params = {'name': 'Simple Template', 'description': 'Simple Board Template'}
+        tasklists_params = [{'title': 'Brainstorm'}, \
+                {'title': 'To Do'}, \
+                {'title': 'Doing'},\
+                {'title': 'Done'}]
+        labels_params = []
+        self.create_template(template_params, tasklists_params, labels_params)
+        return Response(status=status.HTTP_201_CREATED)
+
     @action(detail=False, methods=['get'], url_path='project-management')
     def project_management(self, request):
         template = get_object_or_404(BoardTemplate, name='Project Management')
@@ -94,5 +105,11 @@ class TemplateViewSet(viewsets.GenericViewSet):
     @action(detail=False, methods=['get'], url_path='agile-board')
     def agile_board(self, request):
         template = get_object_or_404(BoardTemplate, name='Agile Board')
+        serializer = self.get_serializer(template)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=['get'], url_path='simple-template')
+    def simple_template(self, request):
+        template = get_object_or_404(BoardTemplate, name='Simple Template')
         serializer = self.get_serializer(template)
         return Response(serializer.data, status=status.HTTP_200_OK)
