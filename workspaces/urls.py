beea420e29f17fdceba46dd38c8ab4d84bf51883
Mohammad Hossein Abbaspour <m_abbaspoor80@comp.iust.ac.ir>
2022-12-20 00:52:37 +0330

    [feat]: get template with id & get all templates

--- workspaces/urls.py
+++ workspaces/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import board, workspace, tasklist, label, task, checklist, comment, templates
+from .views import board, workspace, tasklist, label, task, checklist, templates, comment
 
 
 router = DefaultRouter()
--- workspaces/views/templates.py
+++ workspaces/views/templates.py
@@ -1,14 +1,17 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from ..models import BoardTemplate, TaskList, Label
+from ..models import Board, TaskList, Label
 from ..serializers.boardtemplateserializer import *
 
-class TemplateViewSet(viewsets.GenericViewSet):
-    queryset = BoardTemplate.objects.all()
+
+class TemplateViewSet(ListModelMixin, RetrieveModelMixin, viewsets.GenericViewSet):
     serializer_class = BoardTemplateSerializer
+
+    def get_queryset(self):
+        return Board.objects.filter(is_template=True)
 
     def create_template(self, template_params, tasklists_params, labels_params):
         '''
@@ -18,17 +21,17 @@ class TemplateViewSet(viewsets.GenericViewSet):
         '''
         template_name = template_params.get('name')
         template_description = template_params.get('description')
-        board_template = BoardTemplate.objects.create(name=template_name, description=template_description)
+        board_template = Board.objects.create(name=template_name, description=template_description, is_template=True)
         tasklists = []
         labels = []
         for tasklist_param in tasklists_params:
             tasklist_title = tasklist_param.get('title')
-            tasklists.append(TaskList.objects.create(title=tasklist_title, board_template=board_template))
+            tasklists.append(TaskList.objects.create(title=tasklist_title, board=board_template))
 
         for label_param in labels_params:
             label_title = label_param.get('title')
             label_color = label_param.get('color')
-            labels.append(Label.objects.create(title=label_title, color=label_color, board_template=board_template))
+            labels.append(Label.objects.create(title=label_title, color=label_color, board=board_template))
 
     @action(detail=False, methods=['post'], url_path='create-project-management-template')
     def create_project_management_template(self, request):
@@ -89,27 +92,3 @@ class TemplateViewSet(viewsets.GenericViewSet):
         labels_params = []
         self.create_template(template_params, tasklists_params, labels_params)
         return Response(status=status.HTTP_201_CREATED)
-
-    @action(detail=False, methods=['get'], url_path='project-management')
-    def project_management(self, request):
-        template = get_object_or_404(BoardTemplate, name='Project Management')
-        serializer = self.get_serializer(template)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    @action(detail=False, methods=['get'], url_path='kanban')
-    def kanban(self, request):
-        template = get_object_or_404(BoardTemplate, name='Kanban')
-        serializer = self.get_serializer(template)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    @action(detail=False, methods=['get'], url_path='agile-board')
-    def agile_board(self, request):
-        template = get_object_or_404(BoardTemplate, name='Agile Board')
-        serializer = self.get_serializer(template)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    @action(detail=False, methods=['get'], url_path='simple-template')
-    def simple_template(self, request):
-        template = get_object_or_404(BoardTemplate, name='Simple Template')
-        serializer = self.get_serializer(template)
-        return Response(serializer.data, status=status.HTTP_200_OK)
