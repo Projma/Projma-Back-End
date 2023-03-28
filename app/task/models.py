0de79751b3e0d8092654710a8e4ba1dd05794bd2
MohammadHAbbaspour <mohammadhabp@gmail.com>
2023-03-29 01:50:06 +0330

    [refactor]: seperating task model

--- app/task/models.py
+++ app/task/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from accounts.models import Profile
+from tasklist.models import TaskList
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+    estimate = models.FloatField(blank=True, null=True, default=0.0)
+    spend = models.FloatField(blank=True, null=True, default=0.0)
+    out_of_estimate = models.FloatField(blank=True, null=True)
+    tasklist = models.ForeignKey(TaskList, on_delete=models.CASCADE, related_name='tasks')
+    labels = models.ManyToManyField(to='Label', related_name='tasks', blank=True)
+    doers = models.ManyToManyField(to=Profile, related_name='tasks', blank=True)
+    order = models.IntegerField(default=0, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        creating = self.pk == None
+        self.out_of_estimate = self.spend - self.estimate
+        super().save(*args, **kwargs)
+        if creating:
+            self.order = self.pk
+            self.save()
+        return
+
+    def __str__(self) -> str:
+        return f'{self.title}'
--- app/workspaces/models.py
+++ app/workspaces/models.py
@@ -36,34 +36,6 @@ class WorkSpace(models.Model):
     #     return
 
 
-class Task(models.Model):
-    title = models.CharField(max_length=256)
-    description = models.CharField(max_length=1000, blank=True, null=True)
-    start_date = models.DateField(blank=True, null=True)
-    end_date = models.DateField(blank=True, null=True)
-    estimate = models.FloatField(blank=True, null=True, default=0.0)
-    spend = models.FloatField(blank=True, null=True, default=0.0)
-    out_of_estimate = models.FloatField(blank=True, null=True)
-    tasklist = models.ForeignKey(TaskList, on_delete=models.CASCADE, related_name='tasks')
-    labels = models.ManyToManyField(to='Label', related_name='tasks', blank=True)
-    doers = models.ManyToManyField(to=Profile, related_name='tasks', blank=True)
-    order = models.IntegerField(default=0, null=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
-
-    def save(self, *args, **kwargs):
-        creating = self.pk == None
-        self.out_of_estimate = self.spend - self.estimate
-        super().save(*args, **kwargs)
-        if creating:
-            self.order = self.pk
-            self.save()
-        return
-
-    def __str__(self) -> str:
-        return f'{self.title}'
-
-
 class Comment(models.Model):
     text = models.TextField()
     sender = models.ForeignKey(to=Profile, on_delete=models.CASCADE, related_name='comments')
