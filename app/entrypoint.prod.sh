3f3a226e9a92a68f59897e9b122dab3ac890b2ee
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2023-03-28 19:41:19 +0430

    [feat]: dockerize in prod mode for vps

---
+++ app/entrypoint.prod.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $DB_HOST $DB_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py migrate
+python manage.py collectstatic --no-input
+
+exec "$@"
