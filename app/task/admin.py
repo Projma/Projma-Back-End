33b0e311c52470f7b4d2924fd8e6068310c98066
MohammadHAbbaspour <mohammadhabp@gmail.com>
2023-03-29 16:52:50 +0330

    [refactor]: seperating Comment, CheckList and Attachment admin page and add to task app

--- app/task/admin.py
+++ app/task/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from task.models import Task
+from task.models import Task, CheckList, Comment, Attachment
 
 # Register your models here.
 class TaskAdmin(admin.ModelAdmin):
@@ -9,4 +9,25 @@ class TaskAdmin(admin.ModelAdmin):
     readonly_fields = ['id', 'created_at', 'updated_at', 'out_of_estimate']
 
 
+class CheckListAdmin(admin.ModelAdmin):
+    model = CheckList
+    fields = ['id', 'text', 'is_done', 'task']
+    readonly_fields = ['id']
+
+
+class AttachmentAdmin(admin.ModelAdmin):
+    model = Attachment
+    fields = ['id', 'file', 'task', 'user']
+    readonly_fields = ['id']
+
+
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+    fields = ['id', 'text', 'sender', 'task', 'reply_to', 'created_at', 'updated_at']
+    readonly_fields = ['id', 'created_at', 'updated_at']
+
+
 admin.site.register(Task, TaskAdmin)
+admin.site.register(CheckList, CheckListAdmin)
+admin.site.register(Attachment, AttachmentAdmin)
+admin.site.register(Comment, CommentAdmin)
