13307ac97f871d36b43d867ea1ca4ab421481421
MohammadHAbbaspour <mohammadhabp@gmail.com>
2023-04-03 02:06:16 +0330

    [feat]: initializing Event model

--- app/simple_calendar/models.py
+++ app/simple_calendar/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField(blank=True, null=True)
+    event_time = models.DateTimeField()
+    repeat_duration = models.PositiveIntegerField(default=0)
+    
+    def __str__(self) -> str:
+        return f'{self.title}'
