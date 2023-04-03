35cc26dd1c6d90e126e62b00ea964859ced0682b
MohammadHAbbaspour <mohammadhabp@gmail.com>
2023-04-03 16:08:00 +0330

    [feat]: event urls and api

--- app/simple_calendar/urls.py
+++ app/simple_calendar/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import simplecalendar
+from .views import simplecalendar, event
 
 router = DefaultRouter()
 
-router.register('', simplecalendar.SimpleCalendarViewSet, basename='calendar')
+router.register('simple-calendar', simplecalendar.SimpleCalendarViewSet, basename='calendar')
+router.register('event', event.EventViewSet, basename='event')
 
 urlpatterns = router.urls
