0e6bcc63ce8ac75ee000c3fed337c4cbdc12221b
MohammadHAbbaspour <mohammadhabp@gmail.com>
2023-04-04 17:16:27 +0330

    [fix]: fix bug: start_date and end_date are null when creating task

--- app/task/serializers/taskserializers.py
+++ app/task/serializers/taskserializers.py
@@ -19,8 +19,9 @@ class CreateTaskSerializer(serializers.ModelSerializer):
         model = Task
         fields = ['id', 'created_at', 'updated_at', 'title', 'description', 'start_date', 'end_date', \
                   'estimate', 'spend', 'out_of_estimate', 'tasklist', 'labels', 'doers', 'attachments', 'order']
-        read_only_fields = ['id', 'created_at', 'updated_at', 'description', 'start_date', 'end_date', \
-                  'estimate', 'spend', 'out_of_estimate', 'tasklist', 'labels', 'doers', 'attachments', 'order']
+        # read_only_fields = ['id', 'created_at', 'updated_at', 'description', 'start_date', 'end_date', \
+        #           'estimate', 'spend', 'out_of_estimate', 'tasklist', 'labels', 'doers', 'attachments', 'order']
+        read_only_fields = ['id', 'created_at', 'updated_at', 'out_of_estimate', 'tasklist', 'attachments', 'order']
     
     def validate(self, data):
         if self.instance:
