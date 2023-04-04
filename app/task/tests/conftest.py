ece675497a245cc9b4fd045208ec5768c12efcdd
MohammadHAbbaspour <mohammadhabp@gmail.com>
2023-04-04 17:16:27 +0330

    [fix]: fix bug: start_date and end_date are null when creating task

--- app/task/tests/conftest.py
+++ app/task/tests/conftest.py
@@ -9,14 +9,14 @@ TITLE = "test_task1"
 
 class TaskConf:
     def create_task(api_client:APIClient):
-        def _create_task(tl_id, name=TITLE, start_date=START_DATE, end_date=END_DATE):
-            return api_client.post(f'/task/{tl_id}/create-task/', {'title': name, 'start_date': start_date, 'end_date': end_date})
+        def _create_task(tl_id, title=TITLE, start_date=START_DATE, end_date=END_DATE):
+            return api_client.post(f'/task/{tl_id}/create-task/', {'title': title, 'start_date': start_date, 'end_date': end_date})
         return _create_task
 
 
 @pytest.fixture
 def create_task(api_client:APIClient):
     '''you should create a board then tasklist at first. it returns response(task object)'''
-    def _create_task(tl_id, name=TITLE, start_date=START_DATE, end_date=END_DATE):
-        return TaskConf.create_task(api_client)(tl_id, name, start_date, end_date)
+    def _create_task(tl_id, title=TITLE, start_date=START_DATE, end_date=END_DATE):
+        return TaskConf.create_task(api_client)(tl_id, title, start_date, end_date)
     return _create_task
