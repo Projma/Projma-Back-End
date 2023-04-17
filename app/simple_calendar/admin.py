de68a7d29aab2b9fd617765bd873ad25b14068e1
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2023-04-17 14:37:42 +0330

    [feat]: add MeetingAdmin class for Meeting model

--- app/simple_calendar/admin.py
+++ app/simple_calendar/admin.py
@@ -1,17 +1,25 @@
 from django.contrib import admin
 from .models import *
 
-# Register your models here.
+
 class SimpleCalendarAdmin(admin.ModelAdmin):
     model = SimpleCalendar
     fields = ['id', 'board']
     readonly_fields = ['id']
+
 
 class EventAdmin(admin.ModelAdmin):
     model = Event
     fields = ['id', 'title', 'description', 'event_time', 'repeat_duration', 'event_color', 
               'event_type', 'custom_event_type', 'calendar']
     readonly_fields = ['id']
+
+
+class MeetingAdmin(admin.ModelAdmin):
+    model = Meeting
+    fields = ['id', 'title', 'description', 'start', 'end', 'from_date', 'until_date', 'repeat',
+              'link', 'status', 'created_at', 'updated_at', 'creator', 'calendar', 'color']
+    readonly_fields = ['id', 'created_at', 'updated_at']
 
 
 admin.site.register(SimpleCalendar, SimpleCalendarAdmin)
