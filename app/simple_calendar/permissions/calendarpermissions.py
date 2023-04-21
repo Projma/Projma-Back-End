4e59a103babdfa75524d265effa8d7702cf01b65
MohammadHAbbaspour <mohammadhabp@gmail.com>
2023-04-22 01:34:34 +0330

    [fix]: fix the permission of create calendar and event

--- app/simple_calendar/permissions/calendarpermissions.py
+++ app/simple_calendar/permissions/calendarpermissions.py
@@ -4,6 +4,8 @@ from board.permissions.boardpermissions import *
 class IsCalendarBoardAdmin(IsBoardAdmin):
     def has_object_permission(self, request, view, obj):
         return super().has_object_permission(request, view, obj.board)
+    def has_permission(self, request, view):
+        return super().has_permission(request, view)
 
 
 class IsCalendarBoardMember(IsBoardMember):
