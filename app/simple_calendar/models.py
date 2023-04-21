43a0720cfe16c99e927a1a7c9408051d8153f157
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2023-04-21 16:13:16 +0330

    [feat]: add validations for Meeting model

--- app/simple_calendar/models.py
+++ app/simple_calendar/models.py
@@ -4,6 +4,7 @@ from django.core.exceptions import ValidationError
 from colorfield.fields import ColorField
 from board.models import Board
 from django.db.models import Q
+from datetime import timedelta
 from accounts.models import Profile
 from django.core.validators import MinLengthValidator
 
@@ -23,7 +24,13 @@ class SimpleCalendar(models.Model):
         if not (until_date is None):
             single_meets = single_meets.filter(until_date__lte=until_date)
             periodic_meets = periodic_meets.exclude(from_date__gt=until_date)
-        return  single_meets | periodic_meets
+        if contain_repeats and not (from_date is None or until_date is None):
+            repeats = []
+            for meet in periodic_meets:
+                repeats += meet.duplicate(from_date, until_date)
+            periodic_meets = repeats
+
+        return list(single_meets) + list(periodic_meets)
 
 
 
@@ -75,7 +82,7 @@ class Meeting(models.Model):
     from_date = models.DateField()
     until_date = models.DateField()
     repeat = models.PositiveIntegerField(default=0)
-    link = models.CharField(max_length=512, blank=True)
+    link = models.CharField(max_length=512, blank=True, null=True)
     room_id = models.IntegerField(null=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default=NOTSTARTED)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -84,5 +91,42 @@ class Meeting(models.Model):
     calendar = models.ForeignKey(to=SimpleCalendar, on_delete=models.CASCADE, related_name='meetings')
     color = ColorField()
 
+    def save(self, **kwargs) -> None:
+        creating = self.pk is None
+        if creating:
+            self.status = self.NOTSTARTED
+            self.link = None
+            self.room_id = None
+        super().save(**kwargs)
+
+    def to_single(self, date):
+        singlemeet = Meeting()
+        singlemeet.id = self.id
+        singlemeet.title = self.title
+        singlemeet.description = self.description
+        singlemeet.start = self.start
+        singlemeet.end = self.end
+        singlemeet.from_date = date
+        singlemeet.until_date = date
+        singlemeet.repeat = 0
+        singlemeet.link = self.link
+        singlemeet.room_id = self.room_id
+        singlemeet.status = self.status
+        singlemeet.created_at = self.created_at
+        singlemeet.updated_at = self.updated_at
+        singlemeet.creator = self.creator
+        singlemeet.calendar = self.calendar
+        singlemeet.color = self.color
+        return  singlemeet
+
+    def duplicate(self, from_date, until_date):
+        cur_date = self.from_date
+        duplicates = []
+        while cur_date < from_date:
+            cur_date += timedelta(days=self.repeat)
+        while cur_date <= until_date and cur_date <= self.until_date:
+            duplicates.append(self.to_single(cur_date))
+            cur_date += timedelta(days=self.repeat)
+        return duplicates
 
     
