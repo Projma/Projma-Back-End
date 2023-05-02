969c36ecf8a3cc4dbe93753fc721c9f94cd7b687
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2023-05-02 18:57:31 +0330

    [feat]: config jwt authentication for ws channels

--- app/ProjmaBackend/asgi.py
+++ app/ProjmaBackend/asgi.py
@@ -8,13 +8,26 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from board.channelsmiddleware import JwtAuthMiddlewareStack
 from django.core.asgi import get_asgi_application
+
+import board.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjmaBackend.settings')
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    'http': django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+            JwtAuthMiddlewareStack(
+                URLRouter(
+                    board.routing.websocket_urlpatterns
+                )
+            ),
+        ),
 })
