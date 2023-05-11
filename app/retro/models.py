1d61429c32bb7b69be3a97ad5fea2615525392f5
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2023-05-11 14:34:30 +0330

    [feat]: add retro models

--- app/retro/models.py
+++ app/retro/models.py
@@ -8,9 +8,9 @@ from workspaces.models import WorkSpace
 
 
 class RetroSession(models.Model):
-    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='retro_sessions')
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='retro_sessions', null=True)
     attendees = models.ManyToManyField(Profile, related_name='retro_sessions')
-    admin = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='administrating_retro_sessions')
+    admin = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='administrating_retro_sessions', null=True)
 
 
 class CardGroup(models.Model):
@@ -19,13 +19,14 @@ class CardGroup(models.Model):
 
 
 class RetroCard(models.Model):
-    card_group = models.ForeignKey(CardGroup, on_delete=models.CASCADE, related_name='retro_cards')
+    card_group = models.ForeignKey(CardGroup, on_delete=models.CASCADE, related_name='retro_cards', null=True)
     text = models.CharField(max_length=256)
 
     def init_group(self, session_id):
         session = RetroSession.objects.get(session_id)
-        CardGroup.objects.create(name=self.text, retro_session=session)
-
+        cg = CardGroup.objects.create(name=self.text, retro_session=session)
+        self.card_group = cg
+        self.save()
 
 class RetroReaction(models.Model):
     LIKE = 'LIKE'
@@ -34,5 +35,5 @@ class RetroReaction(models.Model):
         (LIKE, 'Like'),
         (DISLIKE, 'Dislike'),
     ]
-    type = models.CharField()
+    type = models.CharField(max_length=7)
     card_group = models.ForeignKey(CardGroup, on_delete=models.CASCADE, related_name='retro_reactions')
