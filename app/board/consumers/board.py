9438a3732e6df79184e4523b63cd90643c37233c
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2023-05-15 23:52:11 +0330

    [feat]: send message through websocket

---
+++ app/board/consumers/board.py
@@ -0,0 +1,27 @@
+import json
+from channels.generic.websocket import WebsocketConsumer, async_to_sync
+from channels.auth import login
+
+
+class BoardConsumer(WebsocketConsumer):
+    def connect(self):
+        # self.room_group_name = 'X'
+        # async_to_sync(self.channel_layer.group_add)(
+        #     self.room_group_name,
+        #     self.channel_name
+        # )
+        self.send(json.dumps({
+            'type': 'Test Message For Channel Connectivity',
+            'message': '',
+        }))
+
+        self.accept()
+    # async def connect(self):
+    #     for key in self.scope.keys():
+    #         print(key, self.scope[key])
+    #     await self.accept()
+        # await login(self.scope, user)
+        # await database_sync_to_async(self.scope["session"].save)()
+
+    async def disconnect(self, code):
+        pass
