31ad7938543708dced37af4b1e5c5d6ddf730a83
MohammadHAbbaspour <mohammadhabp@gmail.com>
2023-05-17 23:17:36 +0330

    [feat]: IceBreaker serializer

--- app/retro/serializers/sessionserializers.py
+++ app/retro/serializers/sessionserializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from retro.models import RetroSession
+from accounts.serializers import PublicInfoProfileSerializer
 
 
 class CreateSessionSerializer(serializers.ModelSerializer):
@@ -7,3 +8,11 @@ class CreateSessionSerializer(serializers.ModelSerializer):
         model = RetroSession
         fields = ['id', 'board', 'attendees', 'admin', 'vote_limitation', 'retro_step']
         read_only_fields = ['id']
+
+
+class IceBreakerSerializer(serializers.ModelSerializer):
+    attendees = PublicInfoProfileSerializer(many=True)
+    class Meta:
+        model = RetroSession
+        fields = ['id', 'board', 'attendees', 'admin', 'retro_step']
+        read_only_fields = ['id', 'board', 'attendees', 'admin', 'retro_step']
