54d0167ebc520c41b3f7d59bef99a19dbd016be7
MohammadHAbbaspour <mohammadhabp@gmail.com>
2023-05-19 17:07:49 +0330

    [feat]: next step of session real time api in retro

--- app/retro/routing.py
+++ app/retro/routing.py
@@ -1,6 +1,8 @@
 from django.urls import re_path
 from retro.consumers.vote import VoteConsumer
+from retro.consumers.session import SessionConsumer
 
 websocket_urlpatterns = {
     re_path(r'ws/socket-server/vote', VoteConsumer.as_asgi()),
+    re_path(r'ws/socket-server/retro/session/(?P<session_id>\w+)/', SessionConsumer.as_asgi()),
 }
