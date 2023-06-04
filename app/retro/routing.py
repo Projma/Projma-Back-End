963511f8e721c4ee34c822f71b3689d1cefdefd6
MohammadHAbbaspour <mohammadhabp@gmail.com>
2023-06-04 13:16:09 +0330

    [feat]: add grouping step rout

--- app/retro/routing.py
+++ app/retro/routing.py
@@ -2,9 +2,11 @@ from django.urls import re_path
 from retro.consumers.vote import VoteConsumer
 from retro.consumers.session import SessionConsumer
 from retro.consumers.reflect import ReflectConsumer
+from retro.consumers.grouping import GroupConsumer
 
 websocket_urlpatterns = {
     re_path(r'ws/socket-server/vote/(?P<session_id>\w+)/', VoteConsumer.as_asgi()),
     re_path(r'ws/socket-server/retro/session/(?P<session_id>\w+)/', SessionConsumer.as_asgi()),
     re_path(r'ws/socket-server/retro/reflect/(?P<session_id>\w+)/', ReflectConsumer.as_asgi()),
+    re_path(r'ws/socket-server/retro/grouping/(?P<session_id>\w+)/', GroupConsumer.as_asgi()),
 }
