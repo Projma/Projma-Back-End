99079a33c406a55e85846eceefab3d14baef8cef
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2023-06-04 15:40:21 +0330

    [chore]: resolve conflict in reflect.py

--- app/retro/consumers/reflect.py
+++ app/retro/consumers/reflect.py
@@ -1,19 +1,13 @@
 import json
 from asgiref.sync import sync_to_async
+from retro.consumers.session import SessionConsumer
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.auth import login
 from retro.types import RetroSteps
 from retro.models import RetroSession, CardGroup, RetroCard
 
 
-class ReflectConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.USER = self.scope['user']
-        self.SESSION_ID = int(self.scope['url_route']['kwargs']['session_id'])
-        self.GROUP_NAME = "session_%s_reflect" % self.SESSION_ID
-        await self.channel_layer.group_add(self.GROUP_NAME, self.channel_name)
-        await self.accept()
-
+class ReflectConsumer(SessionConsumer):
     @sync_to_async
     def create_card(self, data):
         obj = RetroCard.objects.create(**data)
@@ -23,24 +17,20 @@ class ReflectConsumer(AsyncWebsocketConsumer):
     def get_query(self):
         positive_cards = RetroCard.objects.filter(card_group__retro_session=self.SESSION_ID).filter(is_positive=True).count()
         negative_cards = RetroCard.objects.filter(card_group__retro_session=self.SESSION_ID).filter(is_positive=False).count()
-        return json.dumps({'positive_cnt': positive_cards, 'negative_cnt': negative_cards})
-
-    async def send_nums(self, event):
-        res = await self.get_query()
-        await self.send(res)
+        return {'positive_cnt': positive_cards, 'negative_cnt': negative_cards}
 
     async def receive(self, text_data=None, bytes_data=None):
-        json_data = json.loads(text_data)
-        is_positive = bool(json_data['is_positive'])
-        text = json_data['text']
-        card_obj = {'text': text, 
-                    'is_positive': is_positive}
-        await self.create_card(card_obj)
-        await self.channel_layer.group_send(self.GROUP_NAME,{
-            'type': 'send_nums',
-            'data': card_obj
-        })
-        await self.send(json.dumps({'text': text}))
-
-    async def disconnect(self, code):
-        await self.channel_layer.group_discard(self.GROUP_NAME, self.channel_name)
+        typ, data = await super().receive(text_data, bytes_data)
+        if typ == 'create_card':
+            is_positive = bool(data['is_positive'])
+            text = data['text']
+            card_obj = {'text': text,
+                        'is_positive': is_positive}
+            await self.create_card(card_obj)
+            await self.channel_layer.group_send(self.GROUP_NAME,{
+                'type': 'send_nums',
+                'data': await self.get_query()
+            })
+        
+    async def send_nums(self, event):
+        await self.send(text_data=json.dumps(event))
