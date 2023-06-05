df45d70b7f4bbd159e88a41358b36184196fe733
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2023-06-05 12:59:37 +0330

    [refactor]: edit code for pep8

--- app/retro/serializers/groupserializer.py
+++ app/retro/serializers/groupserializer.py
@@ -3,25 +3,30 @@ from rest_framework import serializers
 from retro.models import CardGroup, RetroCard
 from .cardserializer import SimpleRetroCardSerializer
 
+
 class CardGroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = CardGroup
         fields = ['id', 'name', 'retro_session', 'is_discussed']
 
+
 class GroupsWithCardsSerializer(serializers.ModelSerializer):
     hide = serializers.BooleanField(default=False)
     cards = serializers.SerializerMethodField()
+
     class Meta:
         model = CardGroup
         fields = ['id', 'name', 'cards', 'hide']
-
+        
     def get_cards(self, obj: CardGroup):
         cards = RetroCard.objects.filter(card_group=obj.pk)
         serializer = SimpleRetroCardSerializer(cards, many=True)
         return serializer.data
 
+
 class DiscussCardGroupSerializer(serializers.ModelSerializer):
     votes = serializers.SerializerMethodField()
+
     class Meta:
         model = CardGroup
         fields = ['id', 'name', 'retro_session', 'is_discussed', 'votes']
