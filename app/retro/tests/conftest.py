9624a658bd105a617e55c4705f89a57f515d44ff
MohammadHAbbaspour <mohammadhabp@gmail.com>
2023-06-05 18:30:57 +0330

    [test]: retro crud tests

--- app/retro/tests/conftest.py
+++ app/retro/tests/conftest.py
@@ -9,20 +9,21 @@ VOTE_LIMITATION = 5
 
 class RetroConf:
     def create_retro(api_client:APIClient):
-        def _create_retro(board_id, vote_limitation):
-            # response = api_client.post('/retro/', {'board': board_id, 'vote_limitation': vote_limitation})
-            board = Board.objects.get(pk=board_id)
-            print('----------------', api_client.session)
-            admin = Profile.objects.filter(pk=api_client.user).all()
+        def _create_retro(vote_limitation):
+            response = BoardConf.create_board(api_client)()
+            board = Board.objects.get(pk=response.data['id'])
+            admin = Profile.objects.filter(user__username=USERNAME).all()
             wowner = Profile.objects.filter(pk=board.workspace.owner).all()
             attendees = board.members.all() | board.admins.all() | wowner | admin
-            retro = RetroSession.objects.create(board=board, attendees=attendees, admin=admin)
+            retro = RetroSession.objects.create(board=board, admin=admin.first(), vote_limitation=VOTE_LIMITATION)
+            retro.attendees.set(attendees)
+            retro.save()
             return retro
         return _create_retro
 
 
 @pytest.fixture
 def create_retro(api_client: APIClient):
-    def _create_retro(board_id, vote_limitation=VOTE_LIMITATION):
-        return RetroConf.create_retro(api_client)(board_id, vote_limitation)
+    def _create_retro(vote_limitation=VOTE_LIMITATION):
+        return RetroConf.create_retro(api_client)(vote_limitation)
     return _create_retro
--- app/retro/tests/test_retro_session.py
+++ app/retro/tests/test_retro_session.py
@@ -1,11 +1,40 @@
 from rest_framework import status
+from .conftest import VOTE_LIMITATION, USERNAME
+from retro.models import RetroSession
 import pytest
 
 @pytest.mark.django_db
 class TestCreateRetro:
-    def test_create_retro_with_valid_board_returns_201(self, api_client, create_board, create_retro):
-        response = create_board()
-        board_id = response.data['id']
-        retro = create_retro(board_id)
-        print(response)
-        assert response.status_code == status.HTTP_201_CREATED
+    def test_create_retro(self, api_client, create_retro):
+        retro = create_retro()
+        assert retro.vote_limitation == VOTE_LIMITATION
+        assert retro.admin.user.username == USERNAME
+
+
+@pytest.mark.django_db
+class TestGetRetro:
+    def test_get_valid_retro_returns_200(self, api_client, create_retro):
+        retro = create_retro()
+        response = api_client.get(f'/retro/{retro.pk}/')
+        assert response.data['id'] > 0
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_get_invalid_retro_returns_404(self, api_client, create_retro):
+        retro = create_retro()
+        response = api_client.get(f'/retro/{retro.pk+1}/')
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+
+@pytest.mark.django_db
+class TestDeleteRetro:
+    def test_delete_valid_retro_returns_204(self, api_client, create_retro):
+        retro = create_retro()
+        response = api_client.delete(f'/retro/{retro.pk}/')
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+        retro_cnt = RetroSession.objects.filter(pk=retro.pk).count()
+        assert retro_cnt == 0
+
+    def test_delete_invalid_retro_returns_404(self, api_client, create_retro):
+        retro = create_retro()
+        response = api_client.delete(f'/retro/{retro.pk+1}/')
+        assert response.status_code == status.HTTP_404_NOT_FOUND
