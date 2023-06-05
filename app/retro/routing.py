1d6aba2d9c138188cd08589c5522105199918409
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2023-06-05 15:24:16 +0330

    [feat]: add retro discuss websocket

--- app/retro/routing.py
+++ app/retro/routing.py
@@ -3,10 +3,12 @@ from retro.consumers.vote import VoteConsumer
 from retro.consumers.session import SessionConsumer
 from retro.consumers.reflect import ReflectConsumer
 from retro.consumers.grouping import GroupConsumer
+from retro.consumers.discuss import DiscussConsumer
 
 websocket_urlpatterns = {
     re_path(r'ws/socket-server/retro/vote/(?P<session_id>\w+)/', VoteConsumer.as_asgi()),
     re_path(r'ws/socket-server/retro/session/(?P<session_id>\w+)/', SessionConsumer.as_asgi()),
     re_path(r'ws/socket-server/retro/reflect/(?P<session_id>\w+)/', ReflectConsumer.as_asgi()),
     re_path(r'ws/socket-server/retro/group/(?P<session_id>\w+)/', GroupConsumer.as_asgi()),
+    re_path(r'ws/socket-server/retro/discuss/(?P<session_id>\w+)/', DiscussConsumer.as_asgi()),
 }
