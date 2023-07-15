8646050c3ceb0aab0d5ba527cc06c29838f93ae6
mohammad-osoolian <mohammad_osoolian@comp.iust.ac.ir>
2023-07-15 13:59:45 +0330

    [feat]: config django asgi.py file for deploying with daphne

--- app/ProjmaBackend/asgi.py
+++ app/ProjmaBackend/asgi.py
@@ -8,19 +8,21 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjmaBackend.settings')
+
+import django
+
+from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-
 import retro.routing
 from ProjmaBackend.channelsmiddleware import JwtAuthMiddlewareStack
-from django.core.asgi import get_asgi_application
-
 import board.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProjmaBackend.settings')
 
-django_asgi_app = get_asgi_application()
+
 
 application = ProtocolTypeRouter({
     'http': django_asgi_app,
